68a99cc7e917b38e5cb439e13fd853fdec19f055
javierpb275 <javierpb275@gmail.com>
2021-06-14 18:41:29 +0200

    lesson6: added and used yargs.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
+//Yargs helps you build interactive command line tools, by parsing arguments and generating an elegant user interface:
+const yargs = require('yargs');
 const getNotes = require('./lesson6/notes.js');
 
 //process is a big object with many methods and properties. argv: array that contains all of the arguments provided
@@ -9,3 +11,5 @@ if (command === 'add') {
 } else if (command === 'remove') {
     console.log('removing notes')
 }
+
+console.log(yargs.argv);

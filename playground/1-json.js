3ec73c77de06cdd3d8ed53cf0e4884ab64d38243
javierpb275 <javierpb275@gmail.com>
2021-06-20 16:31:22 +0200

    Storing Data with JSON: used readFileSync() method to read the json file and get a buffer that then we turn into string with toString() and parse it to get a js object and be able to access its properties.

--- playground/1-json.js
+++ playground/1-json.js
@@ -4,7 +4,7 @@ const book = {
     title: 'Hello Paco',
     author: 'Pepe Rodrigo'
 }
-
+/*
 //stringify: turn js object into json:
 const bookJSON = JSON.stringify(book);
 
@@ -17,3 +17,15 @@ console.log(bookObject);
 
 //creates a json file with the bookJSON object written in it.
 fs.writeFileSync('1-json.json', bookJSON)
+*/
+
+//buffer: because what comes back is a buffer which is a way for node.js to represent binary data:
+//readFileSync: reads what is inside the file:
+const dataBuffer = fs.readFileSync('1-json.json');
+
+//toString(): turns the buffer data into string
+const dataJSON = dataBuffer.toString();
+
+const data = JSON.parse(dataJSON);
+
+console.log(data.title);

2368c07ca3a8a5a3e04ecc8ec8e42d43b6390398
javierpb275 <javierpb275@gmail.com>
2021-06-20 16:58:50 +0200

    Storing Data with JSON: used the fs module and json parse and stringify to change the json object in 1-json.json

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
+/*
 const book = {
     title: 'Hello Paco',
     author: 'Pepe Rodrigo'
 }
-/*
+
 //stringify: turn js object into json:
 const bookJSON = JSON.stringify(book);
 
@@ -17,7 +18,7 @@ console.log(bookObject);
 
 //creates a json file with the bookJSON object written in it.
 fs.writeFileSync('1-json.json', bookJSON)
-*/
+
 
 //buffer: because what comes back is a buffer which is a way for node.js to represent binary data:
 //readFileSync: reads what is inside the file:
@@ -29,3 +30,25 @@ const dataJSON = dataBuffer.toString();
 const data = JSON.parse(dataJSON);
 
 console.log(data.title);
+*/
+
+//challenge
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const user = JSON.parse(dataJSON);
+
+const changeJsonObjectProps = (name, planet, age) => {
+    user.name = name;
+    user.planet = planet;
+    user.age = age;
+}
+
+changeJsonObjectProps("Pepe", "Earth2", 40);
+
+const userJSON = JSON.stringify(user);
+
+fs.writeFileSync('1-json.json', userJSON)
+
+
+
+

a2616671a9b7d3cde482ef8a65564aaddc9c25a0
javierpb275 <javierpb275@gmail.com>
2021-06-30 21:47:11 +0200

    object property shorthand and object destructuring

---
+++ lesson10/es6objects.js
@@ -0,0 +1,28 @@
+//Object property shorthand:
+
+const name = 'Pepe';
+const age = 30;
+const user = {
+    name,
+    age
+};
+
+console.log(user);
+
+//Object destructuring:
+
+const product = {
+    label: 'pencil',
+    price: 0.15,
+    stock: 50
+}
+
+const {label: productLabel, price: productPrice, stock: productStock} = product;
+
+console.log(productLabel);
+
+const transaction = (type, {label, stock}) => {
+    console.log(type, label, stock);
+};
+
+transaction('order', product);
